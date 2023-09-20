0ec188adb468381cc941039a55bb055672733ba4
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-20 10:16:14 +0200

    Kan ha fixa problemet

--- App.java
+++ App.java
@@ -5,15 +5,17 @@ public class App {
         Scanner tb = new Scanner(System.in);
         System.out.println("Mata en: Förnamn Efternamn Längd och Ålder");
         String hela_skiten = tb.nextLine();
-        int mängd = hela_skiten.length();
         int space = hela_skiten.indexOf(" ");
         String förnamn = hela_skiten.substring(0, space);
         space = hela_skiten.indexOf(" ",space+1);
-        String eftenamn = hela_skiten.substring(0,space);
+        int mängd = förnamn.length();
+        String eftenamn = hela_skiten.substring(mängd,space);
         space = hela_skiten.indexOf(" ",space+1);
-        String längd = hela_skiten.substring(0,space);
+        mängd = efternamn.length();
+        String längd = hela_skiten.substring(mängd,space);
         space = hela_skiten.indexOf(" ",space+1);
-        String ålder = hela_skiten.substring(0,space);
+        mängd = längd.length();
+        String ålder = hela_skiten.substring(mängd,space);
         System.out.println("Du är "+ förnamn+ " "+ eftenamn+" och du är "+längd+" lång och "+ålder+" gammal");
 
 
